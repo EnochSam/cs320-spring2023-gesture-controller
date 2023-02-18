8aa26da5f161602c1ab14d1d35e6a017069e5315
Enoch Sam <esam@ycp.edu>
2023-02-18 10:02:13 -0500

    Added angle calculation for lists of 3 joints. Added basic fist recognizer using angles

--- src/controller.py
+++ src/controller.py
@@ -4,6 +4,7 @@ import numpy as np
 import uuid
 import os
 
+from matplotlib import pyplot as plt
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
@@ -11,6 +12,75 @@ from mediapipe.tasks.python import vision
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+
+def get_label(index, hand, results):
+    output = None
+    for idx, classification in enumerate(results.multi_handedness):
+        if classification.classification[0].index == index:
+
+            # Process Results
+            label = classification.classification[0].label
+            score = classification.classification[0].score
+            text = '{} {}'.format(label, round(score, 2))
+
+            # Extract Coordinates
+            coords = tuple(np.multiply(
+                           np.array(
+                               (hand.landmark[mp_hands.HandLandmark.WRIST].x, hand.landmark[mp_hands.HandLandmark.WRIST].y)),
+                           [640, 480]).astype(int))
+
+            output = text, coords
+
+            return output
+
+
+def draw_finger_angles(image, results, joint_list):
+
+    # Loop through hands
+    for hand in results.multi_hand_landmarks:
+        # Loop through joint sets
+        for joint in joint_list:
+            # First Coord
+            a = np.array([hand.landmark[joint[0]].x,
+                         hand.landmark[joint[0]].y])
+            # Second Coord
+            b = np.array([hand.landmark[joint[1]].x,
+                         hand.landmark[joint[1]].y])
+            # Third Coord
+            c = np.array([hand.landmark[joint[2]].x,
+                         hand.landmark[joint[2]].y])
+
+            radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - \
+                np.arctan2(a[1] - b[1], a[0] - b[0])
+            angle = np.abs(radians*180.0/np.pi)\
+
+            if angle > 180.0:
+                angle = 360-angle
+
+            cv2.putText(image, str(round(angle, 2)), tuple(np.multiply(b, [640, 480]).astype(
+                int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # if isBent(angle):
+            #     print("joint {} is bent".format(joint))
+
+            angle_list.append(angle)
+
+    return image
+
+
+def isBent(angle):
+    return angle < 150.0
+
+
+# Joint List
+THUMB = [3, 4, 5]
+INDEX = [7, 6, 5]
+MIDDLE = [9, 10, 11]
+RING = [15, 14, 13]
+PINKY = [19, 18, 17]
+joint_list = [INDEX, MIDDLE, RING, PINKY]
+angle_list = []
+isFist = False
 
 # Set up LiveStream
 cap = cv2.VideoCapture(0)
@@ -25,6 +95,9 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         # Set flag
         image.flags.writeable = False
 
+        # Flip image
+        image = cv2.flip(image, 1)
+
         # Detections
         results = hands.process(image)
 
@@ -34,12 +107,10 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         # RGB - BGR
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Flip image
-        # image = cv2.flip(image, 1)
-
         # Detections
-        print(results)
-        print(results.multi_hand_landmarks)
+        # print(results)
+        # print(results.multi_hand_landmarks)
+        # print(results.multi_handedness)
 
         # Render results
         if results.multi_hand_landmarks:
@@ -52,6 +123,20 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
                         color=(121, 44, 250), thickness=2, circle_radius=2),
                 )
 
+                # Render left or right
+                if get_label(num, hand, results):
+                    text, coord = get_label(num, hand, results)
+                    cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX,
+                                1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Draw finger angles
+            draw_finger_angles(image, results, joint_list)
+
+        for angle in angle_list:
+            isFist = isBent(angle)
+
+        if isFist:
+            print("IS fist")
         # Save Images
         # cv2.imwrite(os.path.join('output_images',
         #             '{}.jpg'.format(uuid.uuid1())), image)
@@ -62,25 +147,3 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-# #Base Options
-# BaseOptions = mp.tasks.BaseOptions
-# GestureRecognizer = mp.tasks.vision.GestureRecognizer
-# GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-# GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
-# VisionRunningMode = mp.tasks.vision.VisionRunningMode
-
-# #Create a gesture recognizer instance with live stream mode
-# def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-#     print('gesture recognition result {}'.format(result))
-
-# options = GestureRecognizerOptions(
-#     base_options=BaseOptions(model_asset_path='C:\\Users\\samen\\cs320-spring2023-gesture-controller\\src\\gesture_recognizer.task'),
-#     running_mode=VisionRunningMode.LIVE_STREAM,
-#     result_callback=print_result)
-# with GestureRecognizer.create_from_options(options) as recognizer:
-
-
-# model_path = 'C:\\Users\\samen\\cs320-spring2023-gesture-controller\\src\\gesture_recognizer.task'
-# base_options = BaseOptions(model_asset_path=model_path)

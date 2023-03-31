dbd0709ad7e3d24d59c32f88ce1b2d21f40854c3
Enoch Sam <esam@ycp.edu>
2023-03-31 13:28:06 -0400

    Added johnny test on youtube call

--- src/gesture_recognizer.py
+++ src/gesture_recognizer.py
@@ -1,11 +1,23 @@
+import webbrowser
+import time
 # Gestures
 FIST = "Closed Fist"
 HAND = "Open Hand"
 
 
+def getTime():
+    return time.perf_counter()
+
+
+def elapsed_time(initial):
+    return time.perf_counter() - initial
+
+
 class recognizer:
     def __init__(self) -> None:
         self.gestures = [FIST, HAND]
+        self.Cooldown = 5
+        self.lastCall = getTime()
 
     def getGesture(self, joint_angles):
         isFist = True
@@ -15,6 +27,10 @@ class recognizer:
                 isFist = False
         if isFist:
             gesture = self.getIndex(FIST)
+        elif self.testGesture(joint_angles):
+            gesture = self.getIndex(HAND)
+            if (elapsed_time(self.lastCall) >= self.Cooldown):
+                webbrowser.open("https://www.youtube.com/watch?v=iQwTTRLuEyU")
         else:
             gesture = self.getIndex(HAND)
 
@@ -28,3 +44,9 @@ class recognizer:
             else:
                 index += 1
         return index
+
+    def testGesture(self, angle_list):
+        return (angle_list[0][1] >= 90 and angle_list[1][1] < 90 and angle_list[2][1] < 90 and angle_list[3][1] < 90)
+
+    def isBent(self, angle):
+        return angle < 90
